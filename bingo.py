794d060bd6a47bf718d70d0214a9fb4df8f965aa
Mituo Suzuki <suzumitu@mbox.kyoto-inet.or.jp>
2018-04-17 13:50:18 +0900

    Window icon
     - Set window icon

--- bingo.py
+++ bingo.py
@@ -6,10 +6,13 @@ Created on Fri May  5 23:03:36 2017
 """
 
 import sys
+import os
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import (QWidget, QTableWidgetItem, QApplication)
 from bingoUI import bingo_form
-
 import random
+
+SCRIPTDIR =  os.path.abspath(os.path.dirname(__file__))
 
 class BingoForm(QWidget):
     kth = 0
@@ -28,6 +31,7 @@ class BingoForm(QWidget):
         #    for j in range(10):
         #        item = QTableWidgetItem(str(10*i + j + 1).rjust(5))
         #        self.ui.bingoTable.setItem(i, j, item)
+        self.setWindowIcon(QtGui.QIcon(os.path.join(SCRIPTDIR, 'bingo.ico')))
         self.ui.btnExit.clicked.connect(self.btnExitClicked)
         self.ui.btnNext.clicked.connect(self.next)
 
@@ -58,9 +62,9 @@ class BingoForm(QWidget):
         self.ui.bingoTable.setItem(i, j, item)
         self.ui.bingoTable.item(i, j).setSelected(True)
         self.deselect()
-        
+
         self.kth += 1
-    
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     form = BingoForm()
